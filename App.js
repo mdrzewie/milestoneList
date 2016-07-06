35dd572e50e9674648b8097c9fa1aa81a00a29c4
Mike D <mdrzewie@gmail.com>
2016-07-06 13:48:06 +0000

    Adding actual code since I missed that the last time

--- App.js
+++ App.js
@@ -1,9 +1,39 @@
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
+    
     launch: function() {
-        //Write app code here
-
-        //API Docs: https://help.rallydev.com/apps/2.1/doc/
+        this.add({
+            xtype: 'rallygrid',
+            columnCfgs: [
+                'FormattedID',
+                'Name',
+                'Project'/*,
+                'ScheduleState',
+                'Parent'*/
+            ],
+            context: this.getContext(),
+            /*features: [{
+                ftype: 'groupingsummary',
+                groupHeaderTpl: '{name} ({rows.length})'
+            }],*/
+            storeConfig: {
+                model: 'Milestone',
+                /*groupField: 'Project',
+                groupDir: 'ASC',*/
+                /*filters : [
+                    {
+                        property : 'Name',
+                        operator : 'contains',
+                        value : ' ' 
+                    },
+                ],*/
+                //fetch: ['Project'],
+                /*getGroupString: function(record) {
+                    var owner = record.get('Project');
+                    return (owner && owner._refObjectName) || 'No Project';
+                }*/
+            }
+        });
     }
 });
